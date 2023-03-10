eb1b803f6d36156f6528bf45ba3c1038acdad952
Angelika <123965352+EndziTheNoob@users.noreply.github.com>
2023-03-10 18:11:10 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+/*----JavaScript for Toggle Menu----*/
+
+let navLinks = document.getElementById('navLinks');
+function showMenu() {
+  navLinks.style.right = '0';
+}
+function hideMenu() {
+  navLinks.style.right = '-200px';
+}
+
+// JS for header button
+
+function scrollWin() {
+  window.scrollTo({
+    top: 800,
+    behavior: 'smooth',
+  });
+}
